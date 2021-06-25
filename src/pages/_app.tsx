41440f021b1943ab959b1bdfac065764a8f3526d
Fahad Jabbar <fahadjabbar31@gmail.com>
2021-06-25 19:01:53 +0500

    build: added base url env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import {
 } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql` : `http://localhost:3000/api/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/grahpql`,
 });
 
 const client = new ApolloClient({
